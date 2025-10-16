95c9384e8eb77c1f2ca73a7d7f6919f526a9c2e7
alimusawa313 <90315924+alimusawa313@users.noreply.github.com>
2025-10-17 05:00:02 +0700

    CircleProfileComp added

---
+++ LumenoAi-Test/Router/Router.swift
@@ -0,0 +1,18 @@
+//
+//  Router.swift
+//  LumenoAi-Test
+//
+//  Created by Ali Haidar on 10/17/25.
+//
+
+import SwiftUI
+
+struct Router: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Router()
+}
--- LumenoAi-Test/View/Components/CircleProfileComp.swift
+++ LumenoAi-Test/View/Components/CircleProfileComp.swift
@@ -8,11 +8,64 @@
 import SwiftUI
 
 struct CircleProfileComp: View {
+    
+    var imgSize: CGFloat = 80
+    var imageName: ImageResource
+    var name: String
+    var email: String
+    @State private var isExpanding: Bool = false
+    //    @Binding var isExpanding: Bool
+    //    @EnvironmentObject var router: Router
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: imgSize, height: imgSize)
+                .clipShape(Circle())
+                .shadow(radius: 5, x: 0, y: 2)
+                .onTapGesture {
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                        isExpanding.toggle()
+                    }
+                }
+            
+            if isExpanding{
+                VStack(alignment: .leading, spacing: 8){
+                    Text(name)
+                        .bold()
+                        .foregroundStyle(.primary)
+                    Text("\(email)")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                    
+                    Button("View Profile"){
+                    }
+                    .buttonStyle(.glassProminent)
+                    
+                }
+                .padding(.trailing, 5)
+                .transition(.asymmetric(
+                    insertion: .scale(scale: 0.8).combined(with: .opacity),
+                    removal: .scale(scale: 0.8).combined(with: .opacity)
+                ))
+            }
+        }
+        .padding(isExpanding ? 15 : 0)
+        .background(
+            RoundedRectangle(cornerRadius: isExpanding ? 20 : 200)
+                .foregroundStyle(.thinMaterial)
+        )
+        .shadow(radius: isExpanding ? 5 : 0, y: isExpanding ? 2 : 0)
+        .compositingGroup()
     }
 }
 
 #Preview {
-    CircleProfileComp()
+    CircleProfileComp( imageName: .avatarEx,
+                       name: "John Doe",
+                       email: "johndoe@email.com",
+                       //                       isExpanding: .constant(false)
+    )
 }
